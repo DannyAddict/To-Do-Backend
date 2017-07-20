07e5b97a52cbfd935cd9cf2a38afd4e290af3dd3
Daniel Dong <d.ddong19@gmail.com>
2017-07-20 20:30:34 +0000

    rooted url

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,6 +7,12 @@ class TodoController < ApplicationController
         def edit
             @todo=Todo.find_by_id(params[:id])
         end
-            def delete
-            end
-        end
+    def update 
+        t = Todo.find_by_id(params[:id]) 
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate'] 
+        t.save 
+        redirect_to "/todo/show/#{t.id}" 
+    end
+
+end
