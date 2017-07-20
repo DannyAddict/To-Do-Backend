37b4b91360e1945022b31474199e937aa716f194
Daniel Dong <d.ddong19@gmail.com>
2017-07-20 17:29:32 +0000

    :id works:

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,17 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        todo_id= params[:id]
+        if todo_id == "1"
+        @todo_description = "Eat"
+        @todo_pomodoro_estimate = 2
+        elsif todo_id== "2"
+        @todo_description= "Code"
+        @todo_pomodoro_estimate = 8
+        elsif todo_id== "3"
         @todo_description= "Sleep"
-        @todo_pomodoro_estimate= 2
+        @todo_pomodoro_estimate = 10
+            
+        end
     end
-    end
+end
